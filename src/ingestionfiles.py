6d4444d9d0fb41b13b9baa687ed6e19eedc6406e
amog4 <saiamoghc@gmail.com>
2020-08-16 19:13:50 +0530

    add comments

--- src/ingestionfiles.py
+++ src/ingestionfiles.py
@@ -16,6 +16,7 @@ resource = resource('s3')
 data_path = '{}/../data/'.format(os.path.dirname(__file__))
 print(data_path)
 
+# config from yaml file
 with open('{}/../config/dev.yml'.format(os.path.dirname(__file__)),'rb') as f:
     doc = yaml.load(f,Loader=yaml.FullLoader)
 
@@ -28,14 +29,11 @@ s3path = S3_BASE_KEY.format(name= val['name'],schema = val['schema'])
 s3_ = s3bucket(bucket_name=bucket_name,client = client,resource=resource)
 date_ = str(datetime.datetime.now()).split(' ')[0]
 
-
+# upload files
 for path,subdirs, files in os.walk(data_path):
     print(path)
     for file in files:
-        #print(os.path.join(os.getcwd(),data_path,file))
         with open(os.path.join(path,file),'rb') as d:
-
-
             s3_.uploadfile(path = d.name,key=s3path+file)
 
 
